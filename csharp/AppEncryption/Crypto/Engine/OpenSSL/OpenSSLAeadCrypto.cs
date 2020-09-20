b33335a42594878143379c3c02c872a549f06e2e
Jeremiah Gowdy <jeremiah@gowdy.me>
2020-09-20 07:28:20 -0700

    Get started with OpenSSLAeadCrypto

---
+++ csharp/AppEncryption/Crypto/Engine/OpenSSL/OpenSSLAeadCrypto.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoDaddy.Asherah.Crypto.Engine.OpenSSL
+{
+    class OpenSSLAeadCrypto : AeadCrypto
+    {
+    }
+}
---
+++ csharp/AppEncryption/Crypto/Engine/OpenSSL/OpenSSLAes256GcmCrypto.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoDaddy.Asherah.Crypto.Engine.OpenSSL
+{
+    class OpenSSLAes256GcmCrypto : OpenSSLAeadCrypto
+    {
+    }
+}
